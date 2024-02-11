66eff6352dec619cb14fed70b00d98f3cf6cdfda
sps500 <148262907+sps500@users.noreply.github.com>
2024-02-11 18:04:38 +0200

    ass

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,5 @@
 const sortByDescendingFriendCount = (users) =>
-  users.sort((a, b) => b.friends.length - a.friends.length);
-
+  users.tosorted((a, b) => {})((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
